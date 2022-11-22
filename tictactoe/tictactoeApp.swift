13c894b139b8b3950b44edcd58a902a6601ed6ac
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2022-11-22 20:18:45 +0300

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Ian Nalyanya on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
